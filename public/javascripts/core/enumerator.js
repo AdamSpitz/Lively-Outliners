20d4e52c0450196f113dfd8d573702362a76f42e
Adam Spitz <adam.spitz@gmail.com>
2010-03-01 22:25:27 -0500

    More progress on dog-fooding the core stuff.

---
+++ public/javascripts/core/enumerator.js
@@ -0,0 +1,25 @@
+lobby.transporter.module.create('enumerator', function(thisModule) {
+
+
+thisModule.addSlots(lobby, function(add) {
+
+  add.creator('enumerator', {}, {category: ['collections']}, {comment: 'An Enumerable whose contents are whatever is yielded by calling the specified method.', copyDownParents: [{expression: 'Enumerable'}]});
+
+});
+
+
+thisModule.addSlots(lobby.enumerator, function(add) {
+
+  add.method('initialize', function (obj, methodName) {
+    this._object = obj;
+    this._methodName = methodName;
+  }, {}, {});
+
+  add.method('_each', function (f) {
+    return this._object[this._methodName].call(this._object, f);
+  }, {}, {});
+
+});
+
+
+});
