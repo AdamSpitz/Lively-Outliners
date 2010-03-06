5f82dcd1c0e7b521be19f9b6df1babb96d084eff
Adam Spitz <adam.spitz@gmail.com>
2010-03-06 15:02:41 -0500

    Using the module system for slot_morph.js.

--- public/javascripts/outliners/slot_morph.js
+++ public/javascripts/outliners/slot_morph.js
@@ -1,5 +1,70 @@
-ColumnMorph.subclass("SlotMorph", {
-  initialize: function($super, slot) {
+lobby.transporter.module.create('slot_morph', function(thisModule) {
+
+
+thisModule.addSlots(lobby, function(add) {
+
+  add.method('SlotMorph', function SlotMorph() { Class.initializer.apply(this, arguments); }, {category: ['S']});
+
+  add.method('SlotContentsPointerArrow', function SlotContentsPointerArrow() { Class.initializer.apply(this, arguments); }, {category: ['S']});
+
+});
+
+
+thisModule.addSlots(SlotContentsPointerArrow, function(add) {
+
+  add.data('superclass', ArrowMorph);
+
+  add.creator('prototype', Object.create(ArrowMorph.prototype));
+
+  add.data('type', SlotContentsPointerArrow);
+
+});
+
+
+thisModule.addSlots(SlotContentsPointerArrow.prototype, function(add) {
+
+  add.data('constructor', SlotContentsPointerArrow);
+
+  add.method('initialize', function ($super, slotMorph, fep) {
+    this._slotMorph = slotMorph;
+    this._fixedEndpoint = fep;
+    $super();
+    allArrows.push(this);
+  });
+
+  add.method('slot', function () {return this._slotMorph.slot();});
+
+  add.method('createEndpoints', function () {
+    this.endpoint1 = this._fixedEndpoint;
+    this.endpoint2 = new ArrowEndpoint(this.slot(), this);
+
+    // aaa - blecch, ugly
+    var slotMorph = this._slotMorph;
+    this.endpoint2.wasJustDroppedOnOutliner = function(outliner) {
+      this.wasJustDroppedOn(outliner);
+      slotMorph.setContents(outliner.mirror());
+    };
+  });
+
+});
+
+
+thisModule.addSlots(SlotMorph, function(add) {
+
+  add.data('superclass', ColumnMorph);
+
+  add.creator('prototype', Object.create(ColumnMorph.prototype));
+
+  add.data('type', SlotMorph);
+
+});
+
+
+thisModule.addSlots(SlotMorph.prototype, function(add) {
+
+  add.data('constructor', SlotMorph);
+
+  add.method('initialize', function ($super, slot) {
     $super();
     this._slot = slot;
     this.setPadding(0);
@@ -25,26 +90,26 @@ ColumnMorph.subclass("SlotMorph", {
     this.signatureRow.refreshContent = function() { this.populateSignatureRow(); }.bind(this);
 
     this.updateAppearance();
-  },
+  });
 
-  isMethodThatShouldBeShownAsPartOfTheBox: function() {
+  add.method('isMethodThatShouldBeShownAsPartOfTheBox', function () {
       if (this.slot().isFunctionBody()) { return true; }
       if (! this.slot().isMethod()) { return false; }
       var aaa_LK_slotNamesAttachedToMethods = ['declaredClass', 'methodName'];
       if (Object.newChildOf(enumerator, this.slot().contents(), 'eachNormalSlot').find(function(s) { return ! aaa_LK_slotNamesAttachedToMethods.include(s.name()); })) { return false; }
       return true;
-  },
+  });
 
-  populateSignatureRow: function() {
+  add.method('populateSignatureRow', function () {
     var ms = [this.labelMorph];
     if (this._shouldShowComment || (this.slot().comment && this.slot().comment())) { ms.push(this.commentButton); }
     ms.push(this.signatureRowSpacer);
     var button = this.isMethodThatShouldBeShownAsPartOfTheBox() ? this.sourceButton() : this.contentsPointer();
     ms.push(button);
     this.signatureRow.replaceThingiesWith(ms);
-  },
+  });
 
-  contentsPointer: function() {
+  add.method('contentsPointer', function () {
     var m = this._contentsPointer;
     if (m) { return m; }
     var slot = this.slot();
@@ -68,28 +133,28 @@ ColumnMorph.subclass("SlotMorph", {
     m.setShapeToLookLikeACircle = function() {};
 
     return m;
-  },
+  });
 
-  sourceButton: function() {
+  add.method('sourceButton', function () {
     var m = this._sourceButton;
     if (m) { return m; }
     return this._sourceButton = createButton('M', function(evt) {
       this.toggleSource();
     }.bind(this), 2);
-  },
+  });
 
-  createButton: function(func) {
+  add.method('createButton', function (func) {
     var m = new ButtonMorph(pt(0,0).extent(pt(10,10)));
     
     m.connectModel({model: {Value: null, getValue: function() {return this.Value;}, setValue: function(v) {this.Value = v; if (!v) {func();}}}, setValue: "setValue", getValue: "getValue"});
     return m;
-  },
+  });
 
-  createRow: function(m) {
+  add.method('createRow', function (m) {
     return createLeftJustifiedRow([m], {left: 15, right: 2, top: 2, bottom: 2, between: 0});
-  },
+  });
 
-  sourceMorph: function() {
+  add.method('sourceMorph', function () {
     var m = this._sourceMorph;
     if (m) { return m; }
     var thisSlotMorph = this;
@@ -109,13 +174,13 @@ ColumnMorph.subclass("SlotMorph", {
     m = createInputBox(getter, setter);
     m.horizontalLayoutMode = LayoutModes.SpaceFill;
     return this._sourceMorph = m;
-  },
+  });
 
-  sourceRow: function() {
+  add.method('sourceRow', function () {
     return this._sourceRow || (this._sourceRow = this.createRow(this.sourceMorph()));
-  },
+  });
 
-  annotationMorph: function() {
+  add.method('annotationMorph', function () {
     var m = this._annotationMorph;
     if (m) { return m; }
     m = this._annotationMorph = new ColumnMorph(this).beInvisible();
@@ -125,41 +190,41 @@ ColumnMorph.subclass("SlotMorph", {
     m.addRow(createLabelledNode("Module",        this._moduleMorph     ));
     m.addRow(createLabelledNode("Initialize to", this._initializerMorph));
     return m;
-  },
+  });
 
-  annotationRow: function() {
+  add.method('annotationRow', function () {
     return this._annotationRow || (this._annotationRow = this.createRow(this.annotationMorph()));
-  },
+  });
 
-  commentMorph: function() {
+  add.method('commentMorph', function () {
     var m = this._commentMorph;
     if (m) { return m; }
     var thisSlotMorph = this;
     return this._commentMorph = createInputBox(function( ) { return thisSlotMorph.slot().comment(); },
                                                function(c) { thisSlotMorph.slot().setComment(c); });
-  },
+  });
 
-  commentRow: function() {
+  add.method('commentRow', function () {
     return this._commentRow || (this._commentRow = this.createRow(this.commentMorph()));
-  },
+  });
 
-  toggleSource: function() {
+  add.method('toggleSource', function () {
     this._shouldShowSource = ! this._shouldShowSource;
     this.updateAppearance();
-  },
+  });
 
-  toggleAnnotation: function() {
+  add.method('toggleAnnotation', function () {
     this._shouldShowAnnotation = ! this._shouldShowAnnotation;
     this.updateAppearance();
-  },
+  });
 
-  toggleComment: function(evt) {
+  add.method('toggleComment', function (evt) {
     this._shouldShowComment = ! this._shouldShowComment;
     this.updateAppearance();
     if (this._shouldShowComment) { evt.hand.setKeyboardFocus(this.commentMorph()); }
-  },
+  });
 
-  rename: function(newName, evt) {
+  add.method('rename', function (newName, evt) {
     MessageNotifierMorph.showIfErrorDuring(function() {
       this.slot().rename(newName);
       var outliner = this.outliner();
@@ -172,29 +237,30 @@ ColumnMorph.subclass("SlotMorph", {
         newSlotMorph.sourceMorph().selectAll();
       }
     }.bind(this), evt);
-  },
+  });
 
-  transferUIStateTo: function(otherSlotMorph) {
+  add.method('transferUIStateTo', function (otherSlotMorph) {
     // used after renaming, since it's actually a whole nother slot and slotMorph but we want it to feel like the same one
     otherSlotMorph._shouldShowSource     = this._shouldShowSource;
     otherSlotMorph._shouldShowComment    = this._shouldShowComment;
     otherSlotMorph._shouldShowAnnotation = this._shouldShowAnnotation;
     otherSlotMorph.updateAppearance();
-  },
+  });
 
-     slot: function() { return this._slot; },
-  inspect: function() { return this.slot().name(); },
+  add.method('slot', function () { return this._slot; });
 
-  outliner: function() {
+  add.method('inspect', function () { return this.slot().name(); });
+
+  add.method('outliner', function () {
     return WorldMorph.current().existingOutlinerFor(this.slot().mirror());
-  },
+  });
 
-  moduleName: function() {
+  add.method('moduleName', function () {
     var module = this.slot().module();
     return module ? module.name() : "";
-  },
+  });
 
-  setModuleName: function(n) {
+  add.method('setModuleName', function (n) {
     var module = transporter.module.existingOneNamed(n);
     if (module) { return this.slot().setModule(module); }
     this.world().confirm("The '" + n + "' module does not exist. Create it?", function(b) {
@@ -203,17 +269,17 @@ ColumnMorph.subclass("SlotMorph", {
         this._moduleMorph.changed();
       }
     }.bind(this));
-  },
+  });
 
-  initializationExpression: function() {
+  add.method('initializationExpression', function () {
     return this.slot().initializationExpression();
-  },
+  });
 
-  setInitializationExpression: function(e) {
+  add.method('setInitializationExpression', function (e) {
     this.slot().setInitializationExpression(e);
-  },
+  });
 
-  updateAppearance: function() {
+  add.method('updateAppearance', function () {
     this.labelMorph.refreshText();
     this.signatureRow.refreshContent();
     if (this._commentMorph)    { this._commentMorph.refreshText(); }
@@ -221,71 +287,71 @@ ColumnMorph.subclass("SlotMorph", {
     if (this._moduleMorph)     { this._moduleMorph .refreshText(); }
     this.refreshContent();
     this.updateFill();
-  },
+  });
 
-  updateFill: function() {
+  add.method('updateFill', function () {
     var color = this.slot().isFromACopyDownParent() ? Color.red.lighter().lighter() : Color.gray;
     this.setFill(defaultFillWithColor(color));
-  },
+  });
 
-  refreshContent: function() {
+  add.method('refreshContent', function () {
     var rows = [this.signatureRow];
     if (this._shouldShowAnnotation) { rows.push(this.annotationRow()); }
     if (this._shouldShowComment   ) { rows.push(this.   commentRow()); }
     if (this._shouldShowSource    ) { rows.push(this.    sourceRow()); }
     this.replaceThingiesWith(rows);
-  },
+  });
 
-  wasJustDroppedOnOutliner: function(outliner) {
+  add.method('wasJustDroppedOnOutliner', function (outliner) {
     this.slot().copyTo(outliner.mirror());
     outliner.expander().expand();
     this.remove();
     outliner.updateAppearance();
-  },
+  });
 
-  wasJustDroppedOnCategory: function(categoryMorph) {
+  add.method('wasJustDroppedOnCategory', function (categoryMorph) {
     var newSlot = this.slot().copyTo(categoryMorph.outliner().mirror());
     newSlot.setCategory(categoryMorph.category());
     categoryMorph.expander().expand();
     this.remove();
     categoryMorph.outliner().updateAppearance();
-  },
+  });
 
-  wasJustDroppedOnWorld: function(world) {
+  add.method('wasJustDroppedOnWorld', function (world) {
     var outliner = world.outlinerFor(this.slot().mirror());
     world.addMorphAt(outliner, this.position());
     outliner.expander().expand();
     this.remove();
-  },
+  });
 
-  setModule: function(m, evt) {
+  add.method('setModule', function (m, evt) {
     this.slot().setModule(m);
     this.updateAppearance();
-  },
+  });
 
-  setContents: function(c, evt) {
+  add.method('setContents', function (c, evt) {
     this.slot().setContents(c);
     if (c.isReflecteeFunction()) { this.beCreator(); }
     this.updateAppearance();
-  },
+  });
 
-  beCreator: function() {
+  add.method('beCreator', function () {
     this.slot().beCreator();
     var contentsOutliner = this.world().existingOutlinerFor(this.slot().contents());
     if (contentsOutliner) { contentsOutliner.updateAppearance(); }
     this.updateAppearance();
-  },
+  });
 
-  grabCopy: function(evt) {
+  add.method('grabCopy', function (evt) {
     var newSlot = this.slot().copyTo(reflect({}));
     var newSlotMorph = new SlotMorph(newSlot);
     newSlotMorph.horizontalLayoutMode = LayoutModes.ShrinkWrap;
     newSlotMorph.forceLayoutRejiggering();
     evt.hand.grabMorphWithoutAskingPermission(newSlotMorph, evt);
     return newSlotMorph;
-  },
+  });
 
-  morphMenu: function(evt) {
+  add.method('morphMenu', function (evt) {
     var menu = new MenuMorph([], this);
 
     if (this.slot().rename) {
@@ -343,28 +409,9 @@ ColumnMorph.subclass("SlotMorph", {
     }
 
     return menu;
-  },
+  });
+
 });
 
-ArrowMorph.subclass("SlotContentsPointerArrow", {
-  initialize: function($super, slotMorph, fep) {
-    this._slotMorph = slotMorph;
-    this._fixedEndpoint = fep;
-    $super();
-    allArrows.push(this);
-  },
 
-  slot: function() {return this._slotMorph.slot();},
-
-  createEndpoints: function() {
-    this.endpoint1 = this._fixedEndpoint;
-    this.endpoint2 = new ArrowEndpoint(this.slot(), this);
-
-    // aaa - blecch, ugly
-    var slotMorph = this._slotMorph;
-    this.endpoint2.wasJustDroppedOnOutliner = function(outliner) {
-      this.wasJustDroppedOn(outliner);
-      slotMorph.setContents(outliner.mirror());
-    };
-  },
 });
