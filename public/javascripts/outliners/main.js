3a87bb00153171a6b03d6c0b17fb47ef3535f3ae
Adam Spitz <adam.spitz@gmail.com>
2010-03-06 18:34:23 -0500

    Got rid of world.js.

--- public/javascripts/outliners/main.js
+++ public/javascripts/outliners/main.js
@@ -1,3 +1,10 @@
+// I don't really know what the proper way is to provide my own world menu.
+// So for now I just overwrite the old one. -- Adam
+WorldMorph.addMethods({
+  inspect: function() { return "Lively"; },
+  morphMenu: function(evt) { return this.livelyOutlinersWorldMenu(evt); }
+});
+
 Morph.suppressAllHandlesForever(); // those things are annoying
 startUpdatingAllArrows();
 CreatorSlotMarker.annotateExternalObjects();
