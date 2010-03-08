c232f815fe69f26999eb3c856853d05426ea60c8
Adam Spitz <adam.spitz@gmail.com>
2010-03-07 22:14:50 -0500

    Apparently Firefox isn't as lazy about those prototype slots.

--- public/javascripts/outliners/slot_morph.js
+++ public/javascripts/outliners/slot_morph.js
@@ -97,8 +97,18 @@ thisModule.addSlots(SlotMorph.prototype, function(add) {
       if (this.slot().isFunctionBody()) { return true; }
       if (! this.slot().isMethod()) { return false; }
       var aaa_LK_slotNamesAttachedToMethods = ['declaredClass', 'methodName'];
-      if (Object.newChildOf(enumerator, this.slot().contents(), 'eachNormalSlot').find(function(s) { return ! aaa_LK_slotNamesAttachedToMethods.include(s.name()); })) { return false; }
-      return true;
+      var nonTrivialSlot = Object.newChildOf(enumerator, this.slot().contents(), 'eachNormalSlot').find(function(s) {
+        if (aaa_LK_slotNamesAttachedToMethods.include(s.name())) {return false;}
+
+        // Firefox seems to have a 'prototype' slot on every function (whereas Safari is lazier about it). I think.
+        if (s.name() === 'prototype') {
+          var proto = s.contents();
+          return ! (proto.size() === 0 && proto.parent().reflectee() === Object.prototype);
+        }
+
+        return true;
+      });
+      return ! nonTrivialSlot;
   }, {category: ['source']});
 
   add.method('populateSignatureRow', function () {
