ff908845243a8419e24dd10a9ac28c5fac57a277
Adam Spitz <adam.spitz@gmail.com>
2010-03-16 17:09:41 -0400

    Oops.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,3 @@
-// I don't really know what the proper way is to provide my own world menu.
-// So for now I just overwrite the old one. -- Adam
-WorldMorph.addMethods({
-  inspect: function() { return "Lively"; },
-});
-
 var debugMode = true;
 Morph.suppressAllHandlesForever(); // those things are annoying
 startUpdatingAllArrows();
